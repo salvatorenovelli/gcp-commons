377dd8fd72e9f298a1e062b3bdc7fe682743ccb5
Salvatore Novelli <salvatore.novelli@gmail.com>
2024-01-21 23:14:45 +0000

    Releasing 2.1.0 - Fixed a bug where we'd trow an error if no subscriptions were required

--- src/main/java/com/myseotoolbox/gcpcommons/GcpCommonsPubSubProperties.java
+++ src/main/java/com/myseotoolbox/gcpcommons/GcpCommonsPubSubProperties.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,9 +16,9 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "com.myseotoolbox.gcpcommons.pubsub")
 public class GcpCommonsPubSubProperties {
     private boolean createTopicIfMissing = true;
-    private final List<String> topics;
+    private final List<String> topics = Collections.emptyList();
     private boolean createSubscriptionIfMissing = true;
-    private final Map<String, String> subscriptions;
+    private final Map<String, String> subscriptions = Collections.emptyMap();
 
     public boolean createTopicIfMissing() {
         return createTopicIfMissing;
@@ -24,9 +26,5 @@ public class GcpCommonsPubSubProperties {
 
     public boolean createSubscriptionIfMissing() {
         return createSubscriptionIfMissing;
-    }
-
-    public Map<String, String> getSubscriptions() {
-        return subscriptions;
     }
 }
